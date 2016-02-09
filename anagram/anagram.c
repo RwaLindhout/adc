194e8fba456c5913784b1bc2a827c512409d741e
Younes Moustaghfir <Younesmous@gmail.com>
2016-02-09 14:16:11 +0100

    still not working

--- anagram/anagram.c
+++ anagram/anagram.c
@@ -1,27 +1,3 @@
-/*
- * anagram.c
- * 
- * Copyright 2016 Younes <younes@younes-PNB-series>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -35,21 +11,21 @@ int **mkArray(int size) {
 	return arr;
 }
 
-void **makeHistogram(int **arr) { //Somehow this does still not work
+void makeHistogram(int **arr, int n) { //Somehow this does still not work
 	char c;
 	int i = 0;
 	c = ' ';
-	while(c != '.') {
+	while(i != n) {
 		c = getchar();
 		if(c >= 'a' && c <= 'z') {
 			arr[i][c - 'a']++;
-			arr[i][27]++;
+			arr[i][26]++;
+		}
+		if(c == '.'){
 			i++;
 		}
 	}
-	c = getchar();
 }
-
 
 int main(int argc, char **argv)
 {
@@ -58,29 +34,23 @@ int main(int argc, char **argv)
 	int **normalS;
 	scanf("%d", &n);
 	normalS = mkArray(n);
-	for(i = 0 ; i < n; i++) {
-		makeHistogram(normalS);
-	}
+	makeHistogram(normalS, n);
+	
 	scanf("%d", &m);
 	testS = mkArray(m);
-	for(int j = 0; j < m; j++) {
-		makeHistogram(testS);
-	}
+	makeHistogram(testS, m);
 	
-	for(int i = 0; i < n; m++) {
-		printf("%d\n", normalS[i][26]);
-		}
 	
-	for(int i = 0; i < m; i++) {
-		for(int j = 0; j < n; i++) {
+	for(i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) {
 			for(int k = 0; k < 26; k++) {
 				if(testS[i][k] != normalS[j][k]) {
-					break;
+					return 0;
 				}
-				else if(testS[i][k] == normalS[j][k] && j == n-1 && testS[i][27] == normalS[i][27]) {
+				else if(k == 25 && testS[i][k] == normalS[j][k] && j == n-1 && testS[i][26] == normalS[i][26]) {
 					printf("%d\n", j+1);
 				}
-				else if(testS[i][k] == normalS[j][k] && j != n-1 && testS[i][27] == normalS[i][27]) {
+				else if(k == 25 && testS[i][k] == normalS[j][k] && j != n-1 && testS[i][26] == normalS[i][26]) {
 					printf("%d ", j+1); 
 				}
 			}
@@ -99,4 +69,3 @@ int main(int argc, char **argv)
 	
 	return 0;
 }
-
